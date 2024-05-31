4b948341eef5a9b1df5921e82bc1d85245261a3f
Artem Kanteruk <mr.artem.boss@gmail.com>
2024-06-01 01:24:47 +0300

    Deleted unused import.

--- src/main/java/com/company/weather_aggregator/configuration/AsyncConfiguration.java
+++ src/main/java/com/company/weather_aggregator/configuration/AsyncConfiguration.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.AdviceMode;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
